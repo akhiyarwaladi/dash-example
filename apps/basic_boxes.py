221431f714fc1dafddeab54e3099f56d6c7b3600
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-03-08 10:18:56 +0700

    first want to add oos

---
+++ apps/basic_boxes.py
@@ -0,0 +1,61 @@
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_admin_components as dac
+
+from example_plots import plot_scatter, plot_pie, plot_table, plot_new_regular, plot_sapa_notsapa, plot_plus_minus
+
+basic_boxes_tab = dac.TabItem(id='content_basic_boxes', 
+                              
+    children=html.Div(
+        [
+#            dac.SimpleBox(
+#            	style = {'height': "600px"},
+#                title = "Box 1",
+#                children=[
+#                    dcc.Graph(
+#			id='box-graph',
+#			figure=plot_new_regular(),
+#                        config=dict(displayModeBar=False),
+#                        style={'width': '38vw'}
+#                    )
+#                ]
+#            ),
+            dac.SimpleBox(
+            	style = {'height': "600px", 'width':"900px"},
+                title = "SAPA vs non SAPA store sales",
+                children=[
+                    dcc.Graph(
+                        figure=plot_sapa_notsapa(),
+                        config=dict(displayModeBar=False),
+                        style={'width': '72vw'}
+                    )
+                ]
+            ),
+            dac.SimpleBox(
+                style = {'height': "600px", 'width':"900px"},
+                title = "New vs Regular member sales",
+                children=[
+                    dcc.Graph(
+                        figure=plot_new_regular(),
+                        config=dict(displayModeBar=False),
+                        style={'width': '72vw'}
+                    )
+                ]
+            ),
+            dac.SimpleBox(
+                style = {'height': "600px", 'width':"900px"},
+                title = "Up and Down sales member",
+                children=[
+                    dcc.Graph(
+			id='box-graph',
+                        config=dict(displayModeBar=False),
+                        style={'width': '72vw'}
+                    )
+                ]
+            )
+
+
+        ], 
+        className='column'
+    )
+)
---
+++ example_plots.py
@@ -0,0 +1,154 @@
+import numpy as np 
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objs as go
+
+sapa_notsapa = pd.read_csv('../../out_plot/sapa_notsapa.csv', sep='\t')
+new_regular = pd.read_csv('../../out_plot/new_regular.csv', sep='\t')
+plus_minus = pd.read_csv('../../out_plot/plus_minus.csv', sep='\t')
+plus_minus['diff_sign'] = plus_minus['diff_sign'].astype('str')
+def plot_pie():
+    
+    labels = ['Oxygen','Hydrogen','Carbon_Dioxide','Nitrogen']
+    values = [4500,2000,1053,500]
+    colors = ['#FEBFB3', '#E1396C', '#96D38C', '#D0F9B1']
+    
+    trace = go.Pie(labels=labels, values=values,
+                   hoverinfo='label+percent', textinfo='value', 
+                   textfont=dict(size=20),
+                   marker=dict(colors=colors, 
+                               line=dict(color='#000000', width=2)))
+                                         
+    return dict(data=[trace]) 
+
+def plot_sapa_notsapa():
+	fig = px.line(sapa_notsapa, x='tbto_create_date', y='net_amount', template='presentation', \
+	              text='tbto_amount_final_rp', color='sapa_enable')
+	fig.update_traces(texttemplate='%{text}', textposition='top center')
+	legend_dict = \
+	    legend=dict(
+	            x=0,
+	            y=1,
+	            traceorder="reversed",
+	            title_font_family="Times New Roman",
+	            font=dict(
+	                family="Courier",
+	                size=12,
+	                color="black"
+	            ),
+	            bgcolor="LightSteelBlue",
+	            bordercolor="Black",
+	            borderwidth=2
+	        )
+	fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', margin=\
+	                  {'l':70, 'r':20, 't':30, 'b':70},legend=legend_dict)
+	return fig
+
+def plot_new_regular():
+	fig = px.line(new_regular, x='tbto_create_date', y='tbto_amount_final', template='presentation', \
+	              text='net_amount', color='member_stat')
+	fig.update_traces(texttemplate='%{text}', textposition='top center')
+	legend_dict = \
+	    legend=dict(
+	            x=0,
+	            y=1,
+	            traceorder="reversed",
+	            title_font_family="Times New Roman",
+	            font=dict(
+	                family="Courier",
+	                size=12,
+	                color="black"
+	            ),
+	            bgcolor="LightSteelBlue",
+	            bordercolor="Black",
+	            borderwidth=2
+	        )
+	fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', margin=\
+	                  {'l':70, 'r':20, 't':30, 'b':70},legend=legend_dict)
+	return fig
+
+def plot_plus_minus():
+	fig = px.line(plus_minus, x='wom', y='count_member', template='presentation', \
+	              text='count_member', color='diff_sign')
+	fig.update_traces(texttemplate='%{text}', textposition='top center')
+	legend_dict = \
+	    legend=dict(
+	            x=0,
+	            y=1,
+	            traceorder="reversed",
+	            title_font_family="Times New Roman",
+	            font=dict(
+	                family="Courier",
+	                size=12,
+	                color="black"
+	            ),
+	            bgcolor="LightSteelBlue",
+	            bordercolor="Black",
+	            borderwidth=2
+	        )
+	fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', margin=\
+	                  {'l':70, 'r':20, 't':30, 'b':70},legend=legend_dict)
+	fig.show()
+
+	return fig
+
+
+def plot_scatter(N=50):
+    
+    trace1 = go.Scatter(
+        y = np.random.randn(N),
+        mode='markers',
+        marker=dict(
+            size=16,
+            color = np.random.randn(N), #set color equal to a variable
+            colorscale='Viridis',
+            showscale=True
+        )
+    )
+        
+    return dict(data=[trace1])
+
+def plot_surface():
+    z_data = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/api_docs/mt_bruno_elevation.csv')
+    
+    data = [
+        go.Surface(
+            z=z_data.values,
+            contours=go.surface.Contours(
+                z=go.surface.contours.Z(
+                  show=True,
+                  usecolormap=True,
+                  highlightcolor="#42f462",
+                  project=dict(z=True)
+                )
+            )
+        )
+    ]
+    layout = go.Layout(
+        title='Mt Bruno Elevation',
+        scene=dict(camera=dict(eye=dict(x=1.87, y=0.88, z=-0.64))),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)',
+        margin=dict(
+            l=35,
+            r=20,
+            b=35,
+            t=45
+        )
+    )
+    return go.Figure(data=data, layout=layout)
+
+def plot_table():
+	df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv')
+
+	fig = go.Figure(data=[go.Table(
+	    header=dict(values=list(df.columns),
+	                fill_color='paleturquoise',
+	                align='left'),
+	    cells=dict(values=[df.Rank, df.State, df.Postal, df.Population],
+	               fill_color='lavender',
+	               align='left'))
+	])
+
+
+	return fig
