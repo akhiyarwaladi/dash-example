1d7dc00418e2f381f5097f8b1b3a4980d684024f
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-03-22 01:38:44 +0700

    hadeh

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -11,31 +11,7 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                               
     children=html.Div(
         [
-            dac.TabBox(
-                [
-                    dac.TabBoxHeader(
-                        dac.TabBoxMenu(
-                            [
-                                dac.TabBoxMenuItem(tab_id='tab_box_1_tab1',
-                                                   label='Tab 1'),
-                                dac.TabBoxMenuItem(tab_id='tab_box_1_tab2',
-                                                   label='Tab 2'),
-                                dac.TabBoxMenuItem(tab_id='tab_box_1_tab3',
-                                                   label='Tab 3') 
-                            ],
-                            id='tab_box_1_menu'
-                        ),
-                        collapsible = False,
-                        closable = True,
-                        title="A card with tabs"
-                    ),
-                    dac.TabBoxBody(
-                        id='tab_box_1'
-                    )       
-                ],
-                width=12,
-                elevation=2
-            ),
+
             dac.SimpleBox(
             	style = {'height': "600px", 'width':"900px"},
                 title = "SAPA vs non SAPA store sales",
