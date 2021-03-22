248c09f65227a00ddef0bb4cab15127f550961cb
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-03-22 11:52:38 +0700

    hadeh

--- example_plots.py
+++ example_plots.py
@@ -96,23 +96,23 @@ def plot_new_regular():
 	              text='net_amount', color='member_stat')
 	fig.update_traces(texttemplate='%{text}', textposition='top center', textfont_size=14)
 	fig.update_xaxes(
-	    dtick="M1",
-	    tickformat="%b\n%Y")
+		dtick="M1",
+		tickformat="%b\n%Y")
 		legend_dict = \
-	    legend=dict(
-	            x=0,
-	            y=1,
-	            traceorder="reversed",
-	            title_font_family="Times New Roman",
-	            font=dict(
-	                family="Courier",
-	                size=12,
-	                color="black"
-	            ),
-	            bgcolor="LightGrey",
-	            bordercolor="Black",
-	            borderwidth=2
-	        )
+		legend=dict(
+		        x=0,
+		        y=1,
+		        traceorder="reversed",
+		        title_font_family="Times New Roman",
+		        font=dict(
+		            family="Courier",
+		            size=12,
+		            color="black"
+		        ),
+		        bgcolor="LightGrey",
+		        bordercolor="Black",
+		        borderwidth=2
+		    )
 	fig.update_layout(uniformtext_minsize=8, uniformtext_mode='hide', margin=\
 	                  {'l':70, 'r':20, 't':30, 'b':70},legend=legend_dict)	
 	
