0a0999edbed48a7f96a4f3ab2d02b933a0e2c5d6
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-04-13 19:28:10 +0700

    hadeh

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -11,63 +11,56 @@ from datetime import datetime as dt
 basic_boxes_tab = dac.TabItem(id='content_basic_boxes', 
                               
     children=html.Div([
-            dbc.Row([
-              dbc.Col(
-                dbc.Card(
-                  [
-                      dbc.CardHeader(
+                dbc.Row([
+                    dbc.Col(
+                        dbc.Card([
+                            dbc.CardHeader(
+                                dbc.Row([
+                                    dbc.Col(html.P("Existing vs New Member Sales"), width=4),
+                                    dbc.Col(
+                                        dcc.DatePickerRange(
+                                            id='exist_new_picker',
+                                            min_date_allowed=dt(2020, 1, 1),
+                                            max_date_allowed=dt(2021, 12, 1),
+                                            start_date_placeholder_text="Start Date",
+                                            end_date_placeholder_text="End Date",
+                                            #initial_visible_month=dt(2020, 1, 1),
+                                            start_date=dt(2020, 1, 1),
+                                            end_date=dt(2021, 12, 1)
+                                        ),width=4
+                                    ),   
+                                ],justify="between",),
+                            ),
 
-                        dbc.Row([
-                            dbc.Col(html.P('"Existing vs New Member Sales"'), width=4),
-                            dbc.Col(
-                                dcc.DatePickerRange(
-                                    id='exist_new_picker',
-                                    min_date_allowed=dt(2020, 1, 1),
-                                    max_date_allowed=dt(2021, 12, 1),
-                                    start_date_placeholder_text="Start Date",
-                                    end_date_placeholder_text="End Date",
-                                    #initial_visible_month=dt(2020, 1, 1),
-                                    start_date=dt(2020, 1, 1),
-                                    end_date=dt(2021, 12, 1)
-                                ),width=4
-                            ),   
-                        ],justify="between",),
-
-
-                        ),
-
-                      dbc.CardBody(
-                          [
+                            dbc.CardBody([
                               # html.H5("Card title", className="card-title"),
                               html.P(
                                     dcc.Graph(
                                       id='exist_new_fig',
                                       config=dict(displayModeBar=False),
-                       
+
                                       ),className="card-text",
                               ),
-                          ]),
+                            ]),
 
- 
-                  ])),
-              dbc.Col(
-                dbc.Card(
-                  [
-                      dbc.CardHeader("Increase and Decrease Sales Member"),
-                      dbc.CardBody(
-                          [
-                              # html.H5("Card title", className="card-title"),
-                              html.P(
+
+                        ])),
+                    dbc.Col(
+                        dbc.Card([
+                            dbc.CardHeader("Increase and Decrease Sales Member"),
+                            dbc.CardBody([
+                                # html.H5("Card title", className="card-title"),
+                                html.P(
                                     dcc.Graph(
                                       id='box-graph',
                                       config=dict(displayModeBar=False),
-                       
-                                      ),className="card-text",
-                              ),
-                          ]),
-                  ])),
 
-            ],className="mb-12")
-        ], className='column'
+                                      ),className="card-text",
+                                ),
+                            ]),
+                        ])),
+
+                    ],className="mb-12")
+                ], className='column'
     )
 )
