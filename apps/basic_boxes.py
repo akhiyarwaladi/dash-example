1241e5a5a72c48ee9a36fb1ec87d2cea5bad6f6f
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-04-13 19:16:27 +0700

    hadeh

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -19,9 +19,8 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
 
                         dbc.Row([
                             dbc.Col(html.P('"Existing vs New Member Sales"'), width=4),
-                          
-                            dbc.Col([
-                                html.P('select date range (mm/dd/yyy)'),
+                            dbc.Col(html.P('select date range (mm/dd/yyy)'), {"size": 6, "offset": 3}),
+                            dbc.Col(
                                 dcc.DatePickerRange(
                                     id='exist_new_picker',
                                     min_date_allowed=dt(2020, 1, 1),
@@ -29,9 +28,9 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                                     #initial_visible_month=dt(2020, 1, 1),
                                     start_date=dt(2020, 1, 1),
                                     end_date=dt(2021, 12, 1)
-                                )],width=4
+                                ),width=4
                             ),   
-                        ],justify="justify",),
+                        ],justify="start",),
 
 
                         ),
