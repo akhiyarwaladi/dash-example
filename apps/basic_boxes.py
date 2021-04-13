782bac1e3b0222b800eff3ff3736e4854a518fa3
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-04-13 19:02:04 +0700

    hadeh

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -29,20 +29,21 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                               ),
                           ]),
 
-                      dbc.CardFooter([
-
-                        dbc.Col(html.P('select date range (mm/dd/yyy)'), width=4),
-                        dbc.Col(
-                            dcc.DatePickerRange(
-                                id='exist_new_picker',
-                                min_date_allowed=dt(2020, 1, 1),
-                                max_date_allowed=dt(2021, 12, 1),
-                                #initial_visible_month=dt(2020, 1, 1),
-                                start_date=dt(2020, 1, 1),
-                                end_date=dt(2021, 12, 1)
-                            ),width=4
-                        ),   
-                    ],justify="between",),
+                      dbc.CardFooter(
+                        dbc.Row([
+                            dbc.Col(html.P('select date range (mm/dd/yyy)'), width=4),
+                            dbc.Col(
+                                dcc.DatePickerRange(
+                                    id='exist_new_picker',
+                                    min_date_allowed=dt(2020, 1, 1),
+                                    max_date_allowed=dt(2021, 12, 1),
+                                    #initial_visible_month=dt(2020, 1, 1),
+                                    start_date=dt(2020, 1, 1),
+                                    end_date=dt(2021, 12, 1)
+                                ),width=4
+                            ),   
+                        ],justify="between",),
+                    )
                   ])),
               dbc.Col(
                 dbc.Card(
