238285badb9c9f8dc99040e8331801f389d8f5c6
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-04-25 14:53:06 +0700

    haha

--- app.py
+++ app.py
@@ -326,7 +326,7 @@ def make_plot_callback(date_start, date_end):
 @app.callback(
     [
         Output('sales_fig', 'figure'),
-        Output('sales_table', 'rows')
+        Output('sales_table', 'columns')
     ],
     [
         Input('demo-dropdown', 'value')
