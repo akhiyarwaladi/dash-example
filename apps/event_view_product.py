e2121c51b5928f63c5097760e2ecbd673712f367
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-04-26 13:50:39 +0700

    view product event

--- apps/event_view_product.py
+++ apps/event_view_product.py
@@ -11,35 +11,35 @@ view_product_tab = dac.TabItem(id='content_view_product',
     children=[
         html.H4('View Product Event'),
 
-        html.Div([
-            dbc.Row([
-              dbc.Col(
-                dbc.Card(
-                  [
-                      dbc.CardHeader(""),
-                      dbc.CardBody(
-                          [
-                              # html.H5("Card title", className="card-title"),
-                              html.P(
-                                    plot_view_product1,className="card-text",
-                              ),
-                          ]),
-                  ])),
-              dbc.Col(
-                dbc.Card(
-                  [
-                      dbc.CardHeader(""),
-                      dbc.CardBody(
-                          [
-                              # html.H5("Card title", className="card-title"),
-                              html.P(
-                                    plot_view_product2,className="card-text",
-                              ),
-                          ]),
-                  ])),
+        # html.Div([
+        #     dbc.Row([
+        #       dbc.Col(
+        #         dbc.Card(
+        #           [
+        #               dbc.CardHeader(""),
+        #               dbc.CardBody(
+        #                   [
+        #                       # html.H5("Card title", className="card-title"),
+        #                       html.P(
+        #                             plot_view_product1,className="card-text",
+        #                       ),
+        #                   ]),
+        #           ])),
+        #       dbc.Col(
+        #         dbc.Card(
+        #           [
+        #               dbc.CardHeader(""),
+        #               dbc.CardBody(
+        #                   [
+        #                       # html.H5("Card title", className="card-title"),
+        #                       html.P(
+        #                             plot_view_product2,className="card-text",
+        #                       ),
+        #                   ]),
+        #           ])),
 
 
-            ],className="md-12")
-        ], className='column')
+        #     ],className="md-12")
+        # ], className='column')
     ]
 )
