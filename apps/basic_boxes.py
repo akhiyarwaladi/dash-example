911db27c41934c3b6fd7109000dd6b0d55c72f3a
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-03 16:43:38 +0700

    hadu

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -25,8 +25,9 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                                             start_date_placeholder_text="Start Date",
                                             end_date_placeholder_text="End Date",
                                             #initial_visible_month=dt(2020, 1, 1),
-                                            start_date=dt(2020, 1, 1),
-                                            end_date=dt(2021, 12, 1)
+                                            display_format='DD MMMM Y',
+                                            #start_date=dt(2020, 1, 1),
+                                            #end_date=dt(2021, 12, 1)
                                         ),width=4
                                     ),   
                                 ],justify="between",),
