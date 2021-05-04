e82c8d404deba66d0c2bff944a5b8b7efc8249f8
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-04 13:48:18 +0700

    add datetime

---
+++ data_loader.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def get_vp():
+	vp = pd.read_csv('/home/server/gli-data-science/akhiyar/out_plot/view_product.csv')
+	vp['All'] = vp.sum(axis=1)
+
+	return vp, round(vp.mean(axis=1).mean(), 2), [{'label': x, 'value': x} for x in vp.columns[1:]]
+
