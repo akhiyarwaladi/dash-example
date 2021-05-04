21344e627cf6f1078eb3b23ad4a0ec516529ebd0
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-04 10:17:51 +0700

    add datetime

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -20,17 +20,15 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                             dbc.CardHeader(
                                 dbc.Row([
                                     dbc.Col(html.H5("Existing vs New Member Sales"), width=4),
-
                                     dbc.Col(
                                         html.Div([
-                                            html.Div(["select date: ",
+                                            html.Div(["date range: ",
                                             dcc.DatePickerRange(
                                                 id='exist_new_picker',
                                                 min_date_allowed=dt(2020, 1, 1),
                                                 max_date_allowed=dt(2021, 12, 1),
                                                 start_date_placeholder_text="Start Date",
                                                 end_date_placeholder_text="End Date",
-                                                #initial_visible_month=dt(2020, 1, 1),
                                                 display_format='DD-MM-Y',
                                                 start_date=start_picker,
                                                 end_date=end_picker
@@ -57,7 +55,28 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                         ])),
                     dbc.Col(
                         dbc.Card([
-                            dbc.CardHeader(html.H5("Increase and Decrease Sales Member")),
+                            dbc.CardHeader(
+                                dbc.Row([
+                                    dbc.Col(html.H5("Increase and Decrease Sales Member"), width=4),
+                                    dbc.Col(
+                                        html.Div([
+                                            html.Div(["date range: ",
+                                            dcc.DatePickerRange(
+                                                id='exist_new_picker',
+                                                min_date_allowed=dt(2020, 1, 1),
+                                                max_date_allowed=dt(2021, 12, 1),
+                                                start_date_placeholder_text="Start Date",
+                                                end_date_placeholder_text="End Date",
+                                                display_format='DD-MM-Y',
+                                                start_date=start_picker,
+                                                end_date=end_picker
+                                            )],
+                                        style={'width': '100%', 'display': 'inline-block'}), 
+                                        html.Div(id='output-container-date-picker-range')
+                                        ])
+                                    , width=4),   
+                                ], justify="between",),
+                            ),
                             dbc.CardBody([
                                 # html.H5("Card title", className="card-title"),
                                 html.P(
