42c314d08fc3f39675ef33bc68cb4280b0020b58
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-15 16:58:17 +0700

    change color

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -86,7 +86,7 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                                       ),className="card-text",
                                 ),
                             ]),
-                        ], color="light")),
+                        ], color="primary")),
 
                     ],className="mb-12")
                 ], className='column'
