06784bca7d9f6b00501b27a3ceabeea437ff13bc
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-15 20:01:35 +0700

    change color

--- apps/basic_boxes.py
+++ apps/basic_boxes.py
@@ -52,7 +52,7 @@ basic_boxes_tab = dac.TabItem(id='content_basic_boxes',
                             ]),
 
 
-                        ], color="light", style={'padding': '25px','font-family': 'Proxima Nova Bold'})),
+                        ], color="light", style={'font-size': '100%', 'font-family': 'Proxima Nova Bold'})),
                     dbc.Col(
                         dbc.Card([
                             dbc.CardHeader(
