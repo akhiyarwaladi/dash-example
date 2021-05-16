7b8c99270d468f65a8e850d91e11f6868a3c3d25
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-16 23:34:40 +0700

    add separate folder and modified one tab

--- loader/competitive_load.py
+++ loader/competitive_load.py
@@ -4,4 +4,5 @@ def get_product_competitive():
 	pc = pd.read_csv('/home/server/gli-data-science/akhiyar/out_plot/product_competitive.csv', sep='\t')
 
 
-	return pc, p_comp['price_vs'].value_counts().to_dict()[True], p_comp['price_vs'].value_counts().to_dict()[False]
+	return pc, pc['price_vs'].value_counts().to_dict()[True], \
+				pc['price_vs'].value_counts().to_dict()[False]
