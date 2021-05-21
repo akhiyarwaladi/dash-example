d3c16be1bb3b4a769a6c29d6c8b2cda725ad5387
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-21 16:03:38 +0700

    gas

--- plots/competitive_plot.py
+++ plots/competitive_plot.py
@@ -97,7 +97,7 @@ def plot_product_competitive():
 	                'filter_query': '{price_vs} = lower',  # matching rows of a hidden column with the id, `id`
 	                'column_id': 'our (Rp.)'
 	            },
-	            'backgroundColor': '#0074D9',
+	            'backgroundColor': '#233e8b',
 	            'color': 'white',
 	        },
 	        {
@@ -105,7 +105,7 @@ def plot_product_competitive():
 	                'filter_query': '{price_vs} = higher',  # matching rows of a hidden column with the id, `id`
 	                'column_id': 'comp (Rp.)'
 	            },
-	            'backgroundColor': '#0074D9',
+	            'backgroundColor': '#233e8b',
 	            'color': 'white',
 	        },
 	        
