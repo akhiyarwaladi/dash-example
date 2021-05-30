eba10fd2d19aa975b43ecd7880d6ed7ee92bae11
Akhiyar Waladi <akhiyarwaladi@gmail.com>
2021-05-31 01:43:01 +0700

    modify sales

--- plots/helper.py
+++ plots/helper.py
@@ -1,4 +1,44 @@
 # assume value is a decimal
+def transform_to_rupiah(value):
+    
+    str_value = str(value)
+    unit = ''
+    show_result = ''
+
+
+    separate_decimal = str_value.split(".")
+    after_decimal = separate_decimal[0]
+    before_decimal = separate_decimal[1]
+
+    reverse = after_decimal[::-1]
+    temp_reverse_value = ""
+
+    for index, val in enumerate(reverse):
+        if (index + 1) % 3 == 0 and index + 1 != len(reverse):
+            temp_reverse_value = temp_reverse_value + val + "."
+        else:
+            temp_reverse_value = temp_reverse_value + val
+
+    temp_result = temp_reverse_value[::-1]
+
+    if len(str_value) >= 12:
+        unit = 'm'
+        show_result = temp_result.split('.')[0]
+    elif len(str_value) >= 8:
+        unit = 'jt'
+        show_result = temp_result.split('.')[0]
+    elif len(str_value) >= 5:
+        unit = 'rb'
+        show_result = temp_result.split('.')[0]
+    else:
+        unit = ''
+        show_result = temp_result.split('.')[0]
+
+
+    return "Rp " + show_result + unit
+
+
+# assume value is a decimal
 def transform_to_rupiah_format(value):
     
     str_value = str(value)
